f4d242e04abe3151689bf26f6bd7531ba35288c9
Caio Moizés Santos <caiomoizess2014@gmail.com>
2022-10-11 11:15:41 -0300

    primeira página

---
+++ pages/index.js
@@ -0,0 +1,123 @@
+
+function Home() {
+    return (
+        <html>
+            <head>
+	            <title>Challengers Stage</title>
+            </head>
+            <body>
+                <div class="container">
+                    <div class="box">
+                        <span class="grupo">Grupo B</span>
+                    </div>
+                    <table>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>Nº</th>
+                            <th style={{width: '400px'}}>Equipe</th>
+                            <th style={{width: '100px'}}>Vitórias</th>
+                            <th style={{width: '100px'}}>Derrotas</th>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>1</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>2</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>3</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>4</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>5</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>6</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>7</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>8</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>9</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>10</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>11</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>12</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>13</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>14</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>15</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                        <tr class="box">
+                            <th style={{width: '40px'}}>16</th>
+                            <td style={{width: '400px'}}>Equipe 1</td>
+                            <td style={{width: '100px'}}>0</td>
+                            <td style={{width: '100px'}}>0</td>
+                        </tr>
+                    </table>
+                </div>
+            </body>
+        </html>
+    )
+}
+
+export default Home
